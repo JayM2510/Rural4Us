adaf0da77309d31458a464ab34916da674e0de8a
JayadityaM10 <jayaditya2015404@akgec.ac.in>
2022-09-24 23:33:30 +0530

    Catalog App

---
+++ flutter_catalog/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
+import 'package:flutter_catalog/utils/routes.dart';
+import 'package:flutter_catalog/widgets/themes.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
+      debugShowCheckedModeBanner: false,
+      initialRoute: MyRoutes.homeRoute,
+      routes: {
+         "/":(context) => LoginPage(),
+         MyRoutes.homeRoute: (context) => HomePage(),
+         MyRoutes.loginRoute: (context) => LoginPage(),
+      },
+    );
+  }
+}
