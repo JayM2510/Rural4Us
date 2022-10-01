acf53ac1e5e302e06e68d41ec551b8fde1c62070
JayM2510 <jay.mishra2510@gmail.com>
2022-10-01 23:56:00 +0530

    Changing Theme

--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class MyTheme {
   static ThemeData lightTheme(BuildContext context) => ThemeData(
@@ -13,10 +14,18 @@ class MyTheme {
       ));
 
   static ThemeData darkTheme(BuildContext context) => ThemeData(
-        brightness: Brightness.dark,
-      );
+  fontFamily: GoogleFonts.poppins().fontFamily,
+      appBarTheme: AppBarTheme(
+        color: Colors.black,
+        elevation: 0.0,
+        iconTheme: IconThemeData(color: Colors.black),
+        textTheme: Theme.of(context).textTheme,
+      ));
+      
 
   //Colors
   static Color creamColor = Color(0xfff5f5f5);
   static Color darkBluishColor = Color(0xff403b58);
+  static Color darkCreamColor = Vx.gray800;
+  static Color lighBluishColor = Vx.purple400;
 }
