6ddb48623f2e2a6a287ec24ac8b073cf950de127
JayM2510 <jay.mishra2510@gmail.com>
2022-10-05 23:43:33 +0530

    Get items in the cart

---
+++ lib/models/cart.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_catalog/models/catalog.dart';
+
+class CartModel {
+  // catalog field
+  late CatalogModel _catalog;
+
+  // Collection of IDs - store Ids of each item
+  final List<int> _itemIds = [];
+
+  // Get Catalog
+  CatalogModel get catalog => _catalog;
+
+  set catalog(CatalogModel newCatalog) {
+    assert(newCatalog != null);
+    _catalog = newCatalog;
+  }
+
+  // Get items in the cart
+  List<Item> get items => _itemIds.map((id) => _catalog.getById(id)).toList();
+}
--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -3,11 +3,11 @@ import 'dart:convert';
 class CatalogModel {
    static late List<Item> items;
 
-   static Item getById(int id) =>
+    Item getById(int id) =>
       items.firstWhere((element) => element.id == id, orElse: null);
 
   // Get Item by position
-  static Item getByPosition(int pos) => items[pos];
+   Item getByPosition(int pos) => items[pos];
 }
 
 
