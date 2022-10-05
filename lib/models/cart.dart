1bc57d87de74e8da708d575095b27bde5fbdd04e
JayM2510 <jay.mishra2510@gmail.com>
2022-10-05 23:46:41 +0530

    Cart Model

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -17,4 +17,20 @@ class CartModel {
 
   // Get items in the cart
   List<Item> get items => _itemIds.map((id) => _catalog.getById(id)).toList();
+
+ // Get total price
+  num get totalPrice =>
+      items.fold(0, (total, current) => total + current.price);
+
+  // Add Item
+
+  void add(Item item) {
+    _itemIds.add(item.id);
+  }
+
+  // Remove Item
+
+  void remove(Item item) {
+    _itemIds.remove(item.id);
+  }
 }
